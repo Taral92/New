d3aa31d84211bc3e57639d5225a177a1a43318d9
Taral Babubhai Patel <taralbabubhaipatel@Tarals-MacBook-Air.local>
2024-08-11 13:59:48 +0530

    Updates

---
+++ precache-manifest.8459937e4dc5e021ff0cf17a99cb09b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb20160d29a1bf2d6613da4e59d0bb2",
+    "url": "/your-repo-name/index.html"
+  },
+  {
+    "revision": "ea123f62f51fb92195e9",
+    "url": "/your-repo-name/static/css/main.fe1e5206.chunk.css"
+  },
+  {
+    "revision": "019e3d16fc38869ce307",
+    "url": "/your-repo-name/static/js/2.d46a0b80.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/your-repo-name/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "ea123f62f51fb92195e9",
+    "url": "/your-repo-name/static/js/main.f9fdb031.chunk.js"
+  },
+  {
+    "revision": "03d6b5f04683543f8cdf",
+    "url": "/your-repo-name/static/js/runtime~main.c9d24b64.js"
+  }
+]);
